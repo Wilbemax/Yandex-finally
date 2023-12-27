612cbd6f63d19df88f067ccf2b7f3dcbae67e32d
pa7lux <pa7lux@gmail.com>
2023-12-27 15:42:45 +0400

    После урока про пропс

--- app/page.js
+++ app/page.js
@@ -1,14 +1,19 @@
 import { Banner } from "./components/Banner/Banner";
-import { PopularCardsList } from "./components/CardsList/PopularCardsList";
-import { NewCardsList } from "./components/CardsList/NewCardsList";
+import { CardsList } from "./components/CardsList/CardsList";
+import { NewCardsFragment } from "./components/CardsList/NewCardsFragment";
+import { PopularCardsFragment } from "./components/CardsList/PopularCardsFragment";
 import { Promo } from "./components/Promo/Promo";
 
 export default function Home() {
   return (
     <main className="main">
       <Banner />
-      <PopularCardsList />
-      <NewCardsList />
+      <CardsList id="popular" title="Популярные">
+        <PopularCardsFragment />
+      </CardsList>
+      <CardsList id="new" title="Новинки">
+        <NewCardsFragment />
+      </CardsList>
       <Promo />
     </main>
   );
