2a538746a4a5b85efca32973fcca128c2d8a3c43
pa7lux <pa7lux@gmail.com>
2024-01-11 21:03:29 +0400

    после урока про хуки навигации

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -8,6 +8,7 @@ import { Popup } from "../Popup/Popup";
 import { AuthForm } from "../AuthForm/AuthForm";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const Header = () => {
   const [popupIsOpened, setPopupIsOpened] = useState(false);
@@ -18,44 +19,86 @@ export const Header = () => {
   const closePopup = () => {
     setPopupIsOpened(false);
   };
+
+  const pathname = usePathname();
   return (
     <header className={Styles["header"]}>
-      <Link href="/" className={Styles["logo"]}>
-        <img
-          className={Styles["logo__image"]}
-          src="/images/logo.svg"
-          alt="Логотип Pindie"
-        />
-      </Link>
+      {pathname === "/" ? (
+        <span className={Styles["logo"]}>
+          <img
+            className={Styles["logo__image"]}
+            src="/images/logo.svg"
+            alt="Логотип Pindie"
+          />
+        </span>
+      ) : (
+        <Link href="/" className={Styles["logo"]}>
+          <img
+            className={Styles["logo__image"]}
+            src="/images/logo.svg"
+            alt="Логотип Pindie"
+          />
+        </Link>
+      )}
       <nav className={Styles["menu"]}>
         <ul className={Styles["menu__list"]}>
           <li className={Styles["menu__item"]}>
-            <Link href="/new" className={Styles["menu__link"]}>
+            <Link
+              href="/new"
+              className={`${Styles["menu__link"]} ${
+                pathname === "/new" && Styles["menu__link_active"]
+              }`}
+            >
               Новинки
             </Link>
           </li>
           <li className={Styles["menu__item"]}>
-            <Link href="/popular" className={Styles["menu__link"]}>
+            <Link
+              href="/popular"
+              className={`${Styles["menu__link"]} ${
+                pathname === "/popular" && Styles["menu__link_active"]
+              }`}
+            >
               Популярные
             </Link>
           </li>
           <li className={Styles["menu__item"]}>
-            <Link href="/shooters" className={Styles["menu__link"]}>
+            <Link
+              href="/shooters"
+              className={`${Styles["menu__link"]} ${
+                pathname === "/shooters" && Styles["menu__link_active"]
+              }`}
+            >
               Шутеры
             </Link>
           </li>
           <li className={Styles["menu__item"]}>
-            <Link href="/runners" className={Styles["menu__link"]}>
+            <Link
+              href="/runners"
+              className={`${Styles["menu__link"]} ${
+                pathname === "/runners" && Styles["menu__link_active"]
+              }`}
+            >
               Ранеры
             </Link>
           </li>
           <li className={Styles["menu__item"]}>
-            <Link href="/pixel-games" className={Styles["menu__link"]}>
+            <Link
+              href="/pixel-games"
+              className={`${Styles["menu__link"]} ${
+                pathname === "/pixel-games" && Styles["menu__link_active"]
+              }`}
+            >
               Пиксельные
             </Link>
           </li>
           <li className={Styles["menu__item"]}>
-            <Link href="/tds" className={Styles["menu__link"]}>
+            <Link
+              href="/tds"
+              className={`${Styles["menu__link"]} ${
+                pathname === "/tds" && Styles["menu__link_active"]
+              }`}
+            >
               TDS
             </Link>
           </li>
