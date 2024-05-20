5a360dde08732e811553761368c41d85ce5a1a64
Willbemax <max22042007@yandex.ru>
2024-05-20 20:47:09 +0300

    edit config

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://wbm.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://wbm-back.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
